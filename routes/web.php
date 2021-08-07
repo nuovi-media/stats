007ca48f3c4b23f9de8adcd0d1ffe974885eefb8
Lorenzo Breda <lorenzo@lbreda.com>
2021-08-07 18:34:27 +0200

    :heavy_plus_sign: AdminLTE and Laravel/UI

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth'])->name('admin.')->prefix('admin')->group(function(){
+    Route::get('/', fn () => view('admin/welcome'));
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
